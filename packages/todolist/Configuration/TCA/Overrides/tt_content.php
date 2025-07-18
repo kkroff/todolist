c44e7cabe0f67f28753c75f82679f0b359f815dc
Christoph Jerchel <c.jerchel@gmail.com>
2025-07-18 19:42:52 +0200

    add basic plugin

---
+++ packages/todolist/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    ['Task List', 'todolist_tasks', 'EXT:todolist/Resources/Public/Icons/tx_todolist_task.svg'],
+    'list_type',
+    'todolist'
+);
