65c724990b153a54b89a4ea7f425d58f0843246f
Christoph Jerchel <c.jerchel@gmail.com>
2025-07-18 19:52:37 +0200

    fix plugin icon

--- packages/todolist/ext_tables.php
+++ packages/todolist/ext_tables.php
@@ -1,6 +1,16 @@
 <?php
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+
 defined('TYPO3') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_todolist_task');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_todolist_task');
+
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(IconRegistry::class);
+
+$iconRegistry->registerIcon(
+    'todolist-plugin-tasks',
+    SvgIconProvider::class,
+    ['source' => 'EXT:todolist/Resources/Public/Icons/todo.svg']
+);
